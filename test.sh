99d9b3910aa4c8a87ec33a00f39a87b01818a2e0
Yoz Grahame <yoz@yoz.com>
2024-11-04 20:33:20 -0800

    test.sh: tests correctness of output

--- test.sh
+++ test.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Fetch the name argument to pass to the "hello" command.
 TEST_NAME_ARG=$(buildkite-agent meta-data get test-name-arg)
 
 # Download the "hello" command binary to the "build" folder
-buildkite-agent artifact download build/hello .
+buildkite-agent artifact download hello/hello .
 
 # Run the command with the name argument
-chmod +x build/hello
-./build/hello $TEST_NAME_ARG
+chmod +x hello/hello
+
+OUTPUT=$(./hello/hello $TEST_NAME_ARG)
+
+if [[ $OUTPUT = "Hello, $TEST_NAME_ARG!" ]]
+then
+    echo $OUTPUT
+else
+    echo "INCORRECT OUTPUT: $OUTPUT"
+    exit 1
+fi
