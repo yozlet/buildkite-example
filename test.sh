f09bc4a816263f85420373154c2e7475577e0caf
Yoz Grahame <yoz@yoz.com>
2024-11-06 18:18:44 -0800

    test.sh: better comments

--- test.sh
+++ test.sh
@@ -8,11 +8,13 @@ TEST_NAME_ARG=$(buildkite-agent meta-data get test-name-arg)
 # Download the "hello" command binary to the "build" folder
 buildkite-agent artifact download hello/hello .
 
-# Run the command with the name argument
+# make the command executable
 chmod +x hello/hello
 
+# Run the command with the name argument
 OUTPUT=$(./hello/hello $TEST_NAME_ARG)
 
+# Test the command output and exit with an error if it doesn't match
 if [[ $OUTPUT = "Hello, $TEST_NAME_ARG!" ]]
 then
     echo $OUTPUT
